dffad247b7ffa1299117c8439b0808a081b092aa
MonteeZ <alancaly2002@gmail.com>
2022-07-26 01:47:42 +0200

    added turd block with properties

--- src/main/java/com/MonteZ/modtego/item/ModCreativeModeTab.java
+++ src/main/java/com/MonteZ/modtego/item/ModCreativeModeTab.java
@@ -4,7 +4,7 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
 public class ModCreativeModeTab {
-    public static final CreativeModeTab DZUS_TAB = new CreativeModeTab("Dzus_Panel") {
+    public static final CreativeModeTab DZUS_TAB = new CreativeModeTab("dzus_panel") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(Moditems.TURD.get());
